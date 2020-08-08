3788d2536c08fb485d0f2c3be5e01ff77090d488
Shivam <shivamdubey080@gmail.com>
2020-08-09 00:24:01 +0530

    added comments

--- Collection Framework/ArrayListDemo.java
+++ Collection Framework/ArrayListDemo.java
@@ -1,22 +1,30 @@
-// Program For ArrayList 
+/* Program For ArrayList including type-safety.
+
+*Duplicates object are allowed in ArrayList*
+
+GitHub:@Shivamdubey080
+
+*/
 
 import java.util.ArrayList;
 
 public class ArrayListDemo {
-    public static void main(String[] args) {
-        ArrayList l = new ArrayList();
+    public static void main(final String[] args) {
+        final ArrayList<String> l = new ArrayList<String>(); // creates an empty ArrayList Object
+
+        l.add("A"); // add elements
+        l.add("B");
         l.add("A");
-        l.add(10);
-        l.add("A");
-	l.add("B");
-        l.add(null);
+        l.add(null); // insert null value
+        
         System.out.println(l);
 
-        l.remove (2);
+        l.remove (2); // remove from index 2
         System.out.println(l);
 
-        l.add(2,"M");
+        l.add(2,"M"); // add element M at index 2
         l.add("N");
+
         System.out.println(l);
     }
 }
